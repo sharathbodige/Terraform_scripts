3aa3d89da03fc1ce0a65934214deffc934750b8b
sharathbodige <sharathgoud.bodige@gmail.com>
2025-12-05 15:58:53 +0530

    Update provider.tf

--- 05-VPC-EC2/provider.tf
+++ 05-VPC-EC2/provider.tf
@@ -4,6 +4,6 @@
 provider "aws" {
 
   region     = "us-east-1"
-  access_key = "AKIAQAKSVS3AO4QISV2M"
-  secret_key = "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"
+  access_key = "sorry, i can't share"
+  secret_key = "sorry, i can't share"
 }
